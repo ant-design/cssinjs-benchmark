c0f5c14905710014e15a945cf0e12ddd2fd3533c
MadCcc <1075746765@qq.com>
2023-12-06 15:51:04 +0800

    docs: public path

--- .umirc.ts
+++ .umirc.ts
@@ -1,6 +1,10 @@
 import { defineConfig } from "umi";
 
+const name = 'cssinjs-benchmark';
+
 export default defineConfig({
   npmClient: 'pnpm',
   exportStatic: {},
+  base: process.env.NODE_ENV === 'production' ? `/${name}/` : '/',
+  publicPath: process.env.NODE_ENV === 'production' ? `/${name}/` : '/',
 });
